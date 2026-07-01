599617bb5c3c8d6f59af14fcdeeed256d28de47a
luby404 <ricardokayoca@gmail.com>
2026-07-01 15:34:20 +0100

    adicionando as rotas

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 from .models import init_models
 from app.ext.auth import init_auth
+
+from app.routes import route
 
 app = Flask(__name__)
 app.secret_key = "oewkedas a, sdalsndaçlsda"
 
 init_models()
 init_auth(app)
+
+app.register_blueprint(route)
