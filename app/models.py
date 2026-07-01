76fbf22b1b45429d11fc7ca64f5af86025df72af
luby404 <ricardokayoca@gmail.com>
2026-07-01 21:10:35 +0100

    add pub_id in Models user

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 import uuid
-
+import secrets
 from flask_login import UserMixin
 
 from .ext.database import orm, Model, db
 
 
 class Usuario(Model, UserMixin):
-    email = orm.CharField(max_length=256, unique=True)
+    email  = orm.CharField(max_length=256, unique=True)
+    pub_id = orm.CharField(max_length=256, defaukt=lambda: secrets.token_urlsafe(16))
 
 class Evento(Model):
     usuario = orm.ForeignKeyField(Usuario, backref="eventos")
     nome    = orm.CharField(max_length=256)
-    pub_id  = orm.UUIDField(default=uuid.uuid4)
     data    = orm.TextField(null=True)
 
 
